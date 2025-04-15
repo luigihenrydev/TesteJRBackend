f3ca4967096171cdfbf94b25ac9beea353c64d4c
Luigi Henry C. Araujo <87998061+luigihenrydev@users.noreply.github.com>
2025-04-15 20:41:06 -0300

    implementa InserirTarefas no TarefasController

--- apiToDo/Controllers/TarefasController.cs
+++ apiToDo/Controllers/TarefasController.cs
@@ -33,10 +33,11 @@ namespace apiToDo.Controllers
         {
             try
             {
+                Tarefas Tarefas = new Tarefas(); // Cria uma instância da classe Tarefas.
+                Tarefas.InserirTarefa(Request); // Chama o método InserirTarefa() da classe Tarefa e insere o que foi recebido como parametro na lista.
 
-                return StatusCode(200);
-
-
+                List<TarefaDTO> lstTarefas = Tarefas.lstTarefas(); // Cria uma lista do tipo TarefaDTO e chamo o método lstTarefas() que traz a lista com  a nova modificação.
+                return StatusCode(200, lstTarefas); // Retorna StatusCode 200 e retorna a lista de tarefas atualizada.
             }
 
             catch (Exception ex)
