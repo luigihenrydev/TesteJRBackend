e758e4409cc03c88afca3eaa77b41915cd569335
Luigi Henry C. Araujo <87998061+luigihenrydev@users.noreply.github.com>
2025-04-15 21:16:28 -0300

    refatora a Model Tarefa.cs

--- apiToDo/Models/Tarefas.cs
+++ apiToDo/Models/Tarefas.cs
@@ -7,31 +7,34 @@ namespace apiToDo.Models
 {
     public class Tarefas
     {
+        private List<TarefaDTO> _lstTarefas = new List<TarefaDTO>(); // cria  uma propriedade privada para armazenar a lista de tarefas
+
+        public Tarefas() // método construtor que popula a lista
+        {
+            _lstTarefas.Add(new TarefaDTO
+            {
+                ID_TAREFA = 1,
+                DS_TAREFA = "Fazer Compras"
+            });
+
+            _lstTarefas.Add(new TarefaDTO
+            {
+                ID_TAREFA = 2,
+                DS_TAREFA = "Fazer Atividad Faculdade"
+            });
+
+            _lstTarefas.Add(new TarefaDTO
+            {
+                ID_TAREFA = 3,
+                DS_TAREFA = "Subir Projeto de Teste no GitHub"
+            });
+        }
+
         public List<TarefaDTO> lstTarefas()
         {
             try
             {
-                List<TarefaDTO> lstTarefas = new List<TarefaDTO>();
-
-                lstTarefas.Add(new TarefaDTO
-                {
-                    ID_TAREFA = 1,
-                    DS_TAREFA = "Fazer Compras"
-                });
-
-                lstTarefas.Add(new TarefaDTO
-                {
-                    ID_TAREFA = 2,
-                    DS_TAREFA = "Fazer Atividad Faculdade"
-                });
-
-                lstTarefas.Add(new TarefaDTO
-                {
-                    ID_TAREFA = 3,
-                    DS_TAREFA = "Subir Projeto de Teste no GitHub"
-                });
-
-                return new List<TarefaDTO>();
+                return _lstTarefas; // retorna a lstTarefas
             }
             catch(Exception ex)
             {
@@ -40,27 +43,27 @@ namespace apiToDo.Models
         }
 
 
-        public void InserirTarefa(TarefaDTO Request)
+        public bool InserirTarefa(TarefaDTO Request)
         {
             try
             {
-                List<TarefaDTO> lstResponse = lstTarefas();
-                lstResponse.Add(Request);
+                _lstTarefas.Add(Request); // adiciona a nova tarefa na lista
+                return true;
             }
             catch(Exception ex)
             {
-                throw ex;
+                throw new Exception("Ocorreu um erro ao tentar adicionar  a tarefa", ex);
             }
         }
-        public void DeletarTarefa(int ID_TAREFA)
+        public bool DeletarTarefa(int ID_TAREFA)
         {
             try
             {
-                List<TarefaDTO> lstResponse = lstTarefas();
-                var tarefa = lstResponse.FirstOrDefault(x => x.ID_TAREFA == ID_TAREFA);
+                var tarefa = _lstTarefas.FirstOrDefault(x => x.ID_TAREFA == ID_TAREFA);
                 if (tarefa != null) // Se a Tarefa com o ID recebido no parametro exista (for diferente de nula)
                 {
-                    lstResponse.Remove(tarefa); // remove a tarefa da lista
+                    _lstTarefas.Remove(tarefa); // remove a tarefa da lista
+                    return true; // retorna true caso exclua
                 }
                 else // Caso a Tarefa não exista na lista
                 {
@@ -74,18 +77,18 @@ namespace apiToDo.Models
             }
         }
 
-        public void AtualizarTarefa(TarefaDTO tarefaAtualizada)
+        public bool AtualizarTarefa(TarefaDTO tarefaAtualizada)
         {
             try
             {
-                List<TarefaDTO> lstResponse = lstTarefas();
-                var tarefaNaLista = lstResponse.FirstOrDefault(x => x.ID_TAREFA == tarefaAtualizada.ID_TAREFA);
+                var tarefaNaLista = _lstTarefas.FirstOrDefault(x => x.ID_TAREFA == tarefaAtualizada.ID_TAREFA);
 
                 if (tarefaNaLista != null) // Se existir uma Tarefa com o ID igual ao ID da tarefa recebida no parametro (for diferente de nula)
                 {
-                    var indexDaTarefa = lstResponse.IndexOf(tarefaNaLista); // pega o indice na lstResponse da tarefa que foi recebida no parametro
-                    lstResponse[indexDaTarefa] = tarefaAtualizada; // pega o objeto que tenha o indice do que foi recebido e substitui com o novo
-                  
+                    var indexDaTarefa = _lstTarefas.IndexOf(tarefaNaLista); // pega o indice na _lstTarefas da tarefa que foi recebida no parametro
+                    _lstTarefas[indexDaTarefa] = tarefaAtualizada; // pega o objeto que tenha o indice do que foi recebido e substitui com o novo
+                    return true; // retorna true caso atualiza
+
                 }
                 else // Caso a Tarefa não exista na lista
                 {
